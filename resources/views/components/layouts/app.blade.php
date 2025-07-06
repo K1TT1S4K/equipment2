fc39305d58c498b5df51ef4b48655851490cd639
K1TT1S4K <150118251+K1TT1S4K@users.noreply.github.com>
2025-07-07 02:08:52 +0700

    เพิ่ม css

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,6 +8,8 @@
     <title>Disposal Equipment</title>
 
     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
